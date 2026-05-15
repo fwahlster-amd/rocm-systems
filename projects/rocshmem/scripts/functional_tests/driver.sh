b01005cc52cb379ad84a9b5ed2f1faa7a6b85d9c
Edgar Gabriel <Edgar.Gabriel@amd.com>
2026-05-15 13:33:06 -0500

    add logic to detect wavefront size in script
    and adjust the tests accordingly. Right now, only tile-tests are being
    adjusted.

--- projects/rocshmem/scripts/functional_tests/driver.sh
+++ projects/rocshmem/scripts/functional_tests/driver.sh
@@ -656,22 +656,32 @@ TestTiles() {
   ##############################################################################
   #       | Name                      | Ranks | Workgroups | Threads | Max Message Size #
   ##############################################################################
+
+  # Detect wavefront size based on GPU architecture
+  # gfx1100 and gfx1201 have wavefront size 32, most others have 64
+  WAVE_SIZE=64
+  if command -v rocminfo >/dev/null 2>&1; then
+    if rocminfo | grep -qE "Name:.*(gfx1100|gfx1201)"; then
+      WAVE_SIZE=32
+    fi
+  fi
+
   ExecTest  "tile_put_contiguous"       2       1            1
   ExecTest  "tile_put_rowmajor"         2       1            1
   ExecTest  "tile_put_colmajor"         2       1            1
   ExecTest  "tile_put_arbitrary"        2       1            1
-  ExecTest  "tile_put_wave_contiguous"  2       1            64
-  ExecTest  "tile_put_wg_contiguous"    2       1            1024
-  ExecTest  "tile_put_wg_contiguous"    2       4            1024
+  ExecTest  "tile_put_wave_contiguous"  2       1            $WAVE_SIZE
+  ExecTest  "tile_put_wg_contiguous"    2       1            $((WAVE_SIZE * 16))
+  ExecTest  "tile_put_wg_contiguous"    2       4            $((WAVE_SIZE * 16))
   ExecTest  "tile_get_contiguous"       2       1            1
   ExecTest  "tile_get_rowmajor"         2       1            1
   ExecTest  "tile_get_colmajor"         2       1            1
   ExecTest  "tile_get_arbitrary"        2       1            1
-  ExecTest  "tile_get_wg_contiguous"    2       1            1024
-  ExecTest  "tile_get_wg_contiguous"    2       4            1024
+  ExecTest  "tile_get_wg_contiguous"    2       1            $((WAVE_SIZE * 16))
+  ExecTest  "tile_get_wg_contiguous"    2       4            $((WAVE_SIZE * 16))
   ExecTest  "tile_put_1d"               2       1            1
   ExecTest  "tile_get_1d"               2       1            1
-  ExecTest  "tile_get_wave_contiguous"  2       1            64
+  ExecTest  "tile_get_wave_contiguous"  2       1            $WAVE_SIZE
 }
 
 TestHeatMapRMA() {
